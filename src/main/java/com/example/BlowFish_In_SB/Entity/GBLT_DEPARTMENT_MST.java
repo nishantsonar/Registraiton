c54b6db4a06d1da4be52c916097da7329744f781
Nishant Sonar <nishantsonar9@gmail.com>
2024-06-28 11:11:01 +0530

    First Commit

---
+++ src/main/java/com/example/BlowFish_In_SB/Entity/GBLT_DEPARTMENT_MST.java
@@ -0,0 +1,26 @@
+package com.example.BlowFish_In_SB.Entity;
+
+public class GBLT_DEPARTMENT_MST {
+    /*
+    * gnum_dept_code numeric(5,0) NOT NULL,
+    gstr_dept_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    gstr_hod_code character varying(18) COLLATE pg_catalog."default",
+    gstr_dept_short character varying(5) COLLATE pg_catalog."default",
+    gnum_location_code numeric(4,0),
+    gnum_isvalid numeric(1,0) NOT NULL DEFAULT 1,
+    gnum_seat_id numeric(5,0),
+    gnum_age_limit numeric(3,0),
+    gstr_gender_code character(1) COLLATE pg_catalog."default",
+    gdt_entry_date timestamp without time zone NOT NULL DEFAULT sysdate,
+    gdt_lstmod_date timestamp without time zone,
+    gnum_lstmod_seatid numeric(5,0),
+    gstr_remarks character varying(50) COLLATE pg_catalog."default",
+    gnum_hospital_code numeric(6,0) NOT NULL,
+    gnum_dept_type numeric(1,0),
+    gnum_is_rotationshift_req numeric(1,0) NOT NULL,
+    gdt_eff_date timestamp without time zone,
+    gnum_is_no_due_form_req numeric(1,0) NOT NULL,
+    gnum_is_ward_req numeric(1,0) NOT NULL,
+    gnum_is_academic_dept numeric(1,0) NOT NULL,
+    * */
+}
