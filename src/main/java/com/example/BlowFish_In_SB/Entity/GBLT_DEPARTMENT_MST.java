56de887cf61232bc078604051d886132025cba8e
Nishant Sonar <nishantsonar9@gmail.com>
2024-06-28 15:41:43 +0530

    First Commit

--- src/main/java/com/example/BlowFish_In_SB/Entity/GBLT_DEPARTMENT_MST.java
+++ src/main/java/com/example/BlowFish_In_SB/Entity/GBLT_DEPARTMENT_MST.java
@@ -1,10 +1,29 @@
 package com.example.BlowFish_In_SB.Entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="GBLT_DEPARTMENT_MST", schema = "AHISM")
 public class GBLT_DEPARTMENT_MST {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "gnum_dept_code")
+    private int deptCode;
+    @Column(name = "gstr_dept_name")
+    private String deptName;
+    @Column(name = "gstr_hod_code")
+    private String hodCode;
+    @Column(name = "gstr_dept_short")
+    private String deptShort;
+    @Column(name = "gnum_location_code" )
+    private int locationCode;
+    @Column(name = "gnum_isvalid")
+    private int isValid;
     /*
-    * gnum_dept_code numeric(5,0) NOT NULL,
-    gstr_dept_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    gstr_hod_code character varying(18) COLLATE pg_catalog."default",
+   -- * gnum_dept_code numeric(5,0) NOT NULL,
+    /gstr_dept_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    /gstr_hod_code character varying(18) COLLATE pg_catalog."default",
     gstr_dept_short character varying(5) COLLATE pg_catalog."default",
     gnum_location_code numeric(4,0),
     gnum_isvalid numeric(1,0) NOT NULL DEFAULT 1,
